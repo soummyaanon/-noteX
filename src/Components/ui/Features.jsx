6c9df9a3f1b152224f06eceea0897e63e81376e2
soummyaanon <soumyaranjanpanda910@gmail.com>
2024-09-18 03:40:14 +0530

    Refactor import paths in Features and NoteEditor components

--- src/Components/ui/Features.jsx
+++ src/Components/ui/Features.jsx
@@ -34,7 +34,7 @@ export function Feature() {
         badge={<Badge />}
         src={[
           { src: "/Featurevid.webm", type: "video/webm" },
-          { src: "/Featurevid.mp4", type: "video/mp4" }
+          { src: "/Featurevid.mkv", type: "video/mp4" }
         ]}
         showGradient={false}
         className="scale-[1.75] md:scale-150 lg:scale-[1.75]"
